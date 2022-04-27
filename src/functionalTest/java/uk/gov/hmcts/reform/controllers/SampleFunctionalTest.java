b2c55d0690e8ea394ce7b98bf5a205333a87b8ee
Louise Huyton <l.huyton@kainos.com>
2022-04-27 14:42:15 +0100

    modified smoke and functional tests

--- src/functionalTest/java/uk/gov/hmcts/reform/controllers/SampleFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/controllers/SampleFunctionalTest.java
@@ -1,10 +1,15 @@
 package uk.gov.hmcts.labs.louisehuytonspring2.controllers;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static io.restassured.RestAssured.given;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class SampleFunctionalTest {
@@ -18,7 +23,19 @@ class SampleFunctionalTest {
         RestAssured.baseURI = testUrl;
     }
 
-    protected Response doGetRequest(final String path) {
-        return doGetRequest(path);
+    @Test
+    void shouldReturnBadRequestForEmptyQuery() {
+        Response response = given()
+            .contentType(ContentType.JSON)
+            .when()
+            .get()
+            .then()
+            .extract().response();
+
+        Assertions.assertEquals(200, response.statusCode());
+        Assertions.assertTrue(response.asString().startsWith("Welcome"));
+
     }
+
+   
 }
--- src/smokeTest/java/uk/gov/hmcts/reform/controllers/SampleSmokeTest.java
+++ src/smokeTest/java/uk/gov/hmcts/reform/controllers/SampleSmokeTest.java
@@ -1,27 +1,41 @@
 package uk.gov.hmcts.labs.louisehuytonspring2.controllers;
 
-import org.junit.jupiter.api.DisplayName;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static io.restassured.RestAssured.given;
 
-@WebMvcTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class SampleSmokeTest {
+    protected static final String CONTENT_TYPE_VALUE = "application/json";
 
-    @Autowired
-    private transient MockMvc mockMvc;
+    @Value("${TEST_URL:http://localhost:8080}")
+    private String testUrl;
 
-    @DisplayName("Should welcome upon root request with 200 response code")
-    @Test
-    void welcomeRootEndpoint() throws Exception {
-        MvcResult response = mockMvc.perform(get("/")).andExpect(status().isOk()).andReturn();
-
-        assertThat(response.getResponse().getContentAsString()).startsWith("Welcome");
+    @BeforeEach
+    public void setUp() {
+        RestAssured.baseURI = testUrl;
     }
+
+    @Test
+    void shouldReturnBadRequestForEmptyQuery() {
+        Response response = given()
+            .contentType(ContentType.JSON)
+            .when()
+            .get()
+            .then()
+            .extract().response();
+
+        Assertions.assertEquals(200, response.statusCode());
+        Assertions.assertTrue(response.asString().startsWith("Welcome"));
+
+    }
+
+   
 }
